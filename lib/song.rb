8e0eeef34b58f54ec8a4270d16d6c24ca8bcf3ae
Juan Tenezaca <tenj34@gmail.com>
2018-08-04 22:31:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
     self.all.find {|song|song.name == name}
   end
 
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
 
