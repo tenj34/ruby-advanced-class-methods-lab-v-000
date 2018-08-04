6650a80d703ad2335483a2598d2f145f794c29aa
Juan Tenezaca <tenj34@gmail.com>
2018-08-04 22:32:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)
   end
 
-  def self.alphabetical(name)
+  def self.alphabetical
     self.all.sort_by {|song|song.name}
   end
 end
